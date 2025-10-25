c15f211cd6d95839e27f0beb0b41b067d004b70a
ziadeenyousaf-spec <ziadeenyousaf@outlook.com>
2025-10-25 21:10:16 +0100

    changed index.html

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//const routes = require('/Routers');
+//app.use('/', routes);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
